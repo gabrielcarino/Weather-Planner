ff80c80867c216d47f287541db8b728ff1caaaae
gabriel Carino <gabejcarino@gmail.com>
2022-04-25 16:46:51 -0500

    Update weather data handling

--- src/index.js
+++ src/index.js
@@ -31,7 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   const weekForecastCard = function (forecastData) {
     const forecastCard = document.getElementById("7-day-forecast");
-    console.log(forecastData);
     document.getElementById("plan-container").classList.remove("hidden");
     forecastData.forEach(day => {
       const miniForecastCard = document.createElement("div")
@@ -39,14 +38,34 @@ document.addEventListener("DOMContentLoaded", () => {
       miniForecastCard.className = "mini-forecast"
       miniForecastCard.innerHTML = 
         `<h3>${day.date.toString().slice(4,6)}/${day.date.toString().slice(6)}</h3>
-        <h3>${day.weather}</h3>
+        <h3 class=${day.weather}>${day.weather}</h3>
         <h3>High of ${(day.temp2m.max*(9/5)+32).toFixed(0)}</h3>
-        <h3>Low of ${(day.temp2m.min*(9/5)+32).toFixed(0)}</h3>`
+        <h3>Low of ${(day.temp2m.min*(9/5)+32).toFixed(0)}</h3>
+        <h3 class=${day.wind10m_max}></h3>`
     })
   };
   const dayForecastCard = function (targetDate, prefTemp) {
-
+    readable();
   };
+  //make displayed data readable
+  const readable = function () {
+  Array.from(document.getElementsByClassName("1")).forEach(el => el.innerHTML = "No Wind");
+  Array.from(document.getElementsByClassName("2")).forEach(el => el.innerHTML = "Winds up to 5mph");
+  Array.from(document.getElementsByClassName("3")).forEach(el => el.innerHTML = "Winds up to 15mph");
+  Array.from(document.getElementsByClassName("4")).forEach(el => el.innerHTML = "Winds up to 25mph");
+  Array.from(document.getElementsByClassName("5")).forEach(el => el.innerHTML = "Winds up to 40 mph");
+  Array.from(document.getElementsByClassName("6")).forEach(el => el.innerHTML = "Winds up to 55mph");
+  Array.from(document.getElementsByClassName("7")).forEach(el => el.innerHTML = "Winds up to 75mph");
+  Array.from(document.getElementsByClassName("8")).forEach(el => el.innerHTML = "Hurrican or Tornado Likely");
+
+  Array.from(document.getElementsByClassName("pcloudy")).forEach(el => el.innerHTML = "part. cloud");
+  Array.from(document.getElementsByClassName("mcloudy")).forEach(el => el.innerHTML = "most. cloudy");
+  Array.from(document.getElementsByClassName("lightrain")).forEach(el => el.innerHTML = "light rain");
+  Array.from(document.getElementsByClassName("oshower")).forEach(el => el.innerHTML = "occ. rain");
+  Array.from(document.getElementsByClassName("ishower")).forEach(el => el.innerHTML = "iso. rain");
+  Array.from(document.getElementsByClassName("lightsnow")).forEach(el => el.innerHTML = "light snow");
+  Array.from(document.getElementsByClassName("rainsnow")).forEach(el => el.innerHTML = "sleet");
+  }
 });
 /*
   plan:
